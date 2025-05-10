deee46eacf97557e527961a6aaeebfded8b07e92
bimal009 <pandeybimal616@gmail.com>
2025-05-11 00:46:49 +0545

    Update GitHub Actions workflow and Next.js config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   output: 'export',
-  basePath: '',
   images: {
     unoptimized: true,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
